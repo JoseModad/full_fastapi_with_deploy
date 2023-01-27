58ca0f411bd3f0434a69cdd5639ecf664cf2fa7b
turko2373 <turko1073@gmail.com>
2023-01-27 03:07:08 -0300

    cree las variables de entorno para conectar postgresql

--- app/db/database.py
+++ app/db/database.py
@@ -1,18 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
-import os
 
 
-load_dotenv()
+from core.config import settings
 
-
-USER_CONTRAS = os.getenv("USER_CONTRAS")
-BASE_DE_DATOS = os.getenv("BASE_DE_DATOS")
 
 # Conexion a la base de datos
-SQLALCHEMY_DATABASE_URL = f"postgresql://{USER_CONTRAS}@localhost:5432/{BASE_DE_DATOS}"
+SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 
 # Interaccion con la base de datos
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
